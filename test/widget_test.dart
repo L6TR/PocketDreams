9e93a24de38f2e608e83825c1acb3cf4657022e4
L6TR <slipknot91304@gmail.com>
2025-06-12 18:48:41 +0200

    new new new start

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pocket_dreams/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PocketDreams());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
