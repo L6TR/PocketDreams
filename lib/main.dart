95339bd8e2a9b955f8d35d84081a72ba0ed896fa
L6TR <slipknot91304@gmail.com>
2025-06-21 17:21:14 +0200

    Login creen button

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LoginScreen());
+  runApp(const PocketDreams());
+}
+
+class PocketDreams extends StatelessWidget {
+  const PocketDreams({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Pocket Dreams", home: const LoginScreen());
+  }
 }
 
 class LoginScreen extends StatelessWidget {
@@ -9,25 +18,22 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 250, 175, 195),
-          title: const Text("Pocket Dreams"),
-          foregroundColor: Colors.white,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 250, 175, 195),
+        title: const Text("Pocket Dreams"),
+        foregroundColor: Colors.white,
+      ),
 
-        backgroundColor: Colors.black,
+      backgroundColor: Colors.black,
 
-        body: ElevatedButton(
-          child: Text("Login"),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => MainScreen()),
-            );
-          },
-        ),
+      body: ElevatedButton(
+        child: Text("Login"),
+        onPressed: () {
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => const MainScreen()));
+        },
       ),
     );
   }
@@ -37,44 +43,42 @@ class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 250, 175, 195),
-          title: const Text("Pocket Dreams"),
-          foregroundColor: Colors.white,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 250, 175, 195),
+        title: const Text("Pocket Dreams"),
+        foregroundColor: Colors.white,
+      ),
 
-        backgroundColor: Colors.black,
+      backgroundColor: Colors.black,
 
-        body: Stack(
-          children: [
-            Container(
-              color: Colors.grey,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: const Text("Dreams"),
-              ),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.grey,
+            child: SizedBox(
+              width: 100,
+              height: 100,
+              child: const Text("Dreams"),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
 
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: const Color.fromARGB(255, 250, 175, 195),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 250, 175, 195),
 
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.cloud),
-              label: "Today's Dream",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_month),
-              label: "Calendar",
-            ),
-          ],
-        ),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cloud),
+            label: "Today's Dream",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: "Calendar",
+          ),
+        ],
       ),
     );
   }
