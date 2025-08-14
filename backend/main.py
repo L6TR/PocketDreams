315074e53e3328abbafa7de4c17c370fd03d584f
L6TR <slipknot91304@gmail.com>
2025-08-14 15:30:43 +0200

    Starting to understand Flask
    + insall MySql

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+# helps Flask
+# idk how
 
-@app.route('/api/data', methods=['GET'])
+
+# main page idk
+@app.route("/")
+def index():
+    return"<h1>Main page</h1>"
+
+@app.route("/api/data", methods=["GET"])
 def get_data():
     data = {"message":"backend"}
     return jsonify(data)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
