d1f538c89e4ff50c1505134d6f5a9c609a2d544a
L6TR <slipknot91304@gmail.com>
2025-08-19 23:07:11 +0200

    Base start
    temporary

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,10 @@ class PocketDreams extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Pocket Dreams", home: const LoginScreen());
+    return MaterialApp(
+      title: "Pocket Dreams",
+      home: const /*LoginScreen*/ Base(),
+    );
   }
 }
 
