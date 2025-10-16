f311e5c18c483593b0cf63371b8f7d904c1f4d5a
L6TR <slipknot91304@gmail.com>
2025-10-16 10:25:26 +0200

    Revert "+ login and - overfload"
    This reverts commit 40b6c78b525e74d821e7fbe97528f2f047d3e495.

--- lib/main.dart
+++ lib/main.dart
@@ -88,7 +88,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   Future<void> register() async {
     final response = await http.post(
-      Uri.parse("http://10.1.240.99:5000/api/register"),
+      Uri.parse("http://192.168.0.233:5000/api/register"),
       headers: {"Content-Type": "application/json"},
       body: json.encode({
         "nickname": nicknameController.text,
@@ -103,17 +103,16 @@ class _RegisterScreenState extends State<RegisterScreen> {
     });
 
     if (data["success"]) {
-      Navigator.pushReplacement(
+      /*Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => LoginScreen()),
-      );
+        MaterialPageRoute(builder: (context) => Base()),
+      );*/
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 250, 175, 195),
         title: const Text("Pocket Dreams"),
@@ -279,7 +278,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Future<void> login() async {
     final response = await http.post(
-      Uri.parse("http://10.1.240.99:5000/api/login"),
+      Uri.parse("http://10.0.1.12:5000/api/login"),
       headers: {"Content-Type": "application/json"},
       body: json.encode({
         "nickname": nicknameController.text,
@@ -294,10 +293,7 @@ class _LoginScreenState extends State<LoginScreen> {
     });
 
     if (data["success"]) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => Base()),
-      );
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Base()));
     }
   }
 
@@ -316,7 +312,6 @@ class _LoginScreenState extends State<LoginScreen> {
     }
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 250, 175, 195),
         title: const Text("Pocket Dreams"),
@@ -734,6 +729,8 @@ class _TodaysDreamState extends State<TodaysDream> {
     }
 
     int len = colorList.length;
+    print(colorList);
+    print(Color.fromARGB(255, mixedR.round(), mixedG.round(), mixedB.round()));
     return Color.fromARGB(
       255,
       (mixedR / len).round(),
